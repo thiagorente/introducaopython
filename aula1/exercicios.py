a2550ab7992f9033766fdb2f31c5ed510e165e92
Logon Aluno <logonaluno@local.com>
2019-11-18 19:47:44 -0300

    exercicio 10

--- aula1/exercicios.py
+++ aula1/exercicios.py
@@ -2,16 +2,19 @@ def ex_1(numero_cm):
     #1.	Faça um Programa que converta metros para centímetros.
     return print('{0} centímetros é igual a {1:.2f} metro(s)'.format(str(numero_cm), int(numero_cm) / 100).replace('.', ','))
 
+
 def ex_2(raio_circulo):
     #2.	Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
     pi = 3.14
     area_circulo = (float(raio_circulo)**2) * pi
     return print('A área do círculo de raio igual a {0} é {1}'.format(raio_circulo, area_circulo).replace('.', ','))
 
+
 def ex_3(lado_quadrado):
     #3.	Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
     area_quadrado = float(lado_quadrado)**2
     return print('O dobro da área do quadrado com lado {0} é {1}'.format(lado_quadrado, area_quadrado * 2).replace('.', ','))
+
 
 def calcula_salario(horas, ganho_hora):
     return horas * ganho_hora
@@ -24,6 +27,7 @@ def ex_5():
 
     return print('Seu salário no mês é {:.2f}'.format(calcula_salario(horas_trabalhadas, ganho_hora)).replace('.', ','))
 
+
 def converte_graus(farenheit):
     return (5 * (float(farenheit) -32)) / 9
 
@@ -31,6 +35,7 @@ def ex_6(graus_farenheit):
     #6.	Faça um Programa que peça a temperatura em graus Farenheit, transforme e mostre a temperatura em graus Celsius.
     # C = (5 * (F-32) / 9).
     return print('{0:.1f} graus Farenheit é igual a {1:.1f} graus Celsius'.format(float(graus_farenheit), converte_graus(graus_farenheit)))
+
 
 def calculo_a(primeiro_numero, segundo_numero):
     return (int(primeiro_numero) * 2) * (int(segundo_numero) / 2)
@@ -51,6 +56,7 @@ def ex_7():
     terceiro_numero = float(input('Digite um número real: '))
 
     return print('Cálculo a: {0}\nCálculo b: {1:f}\nCálculo c: {2:f}'.format(calculo_a(primeiro_numero, segundo_numero), calculo_b(primeiro_numero, terceiro_numero), calculo_c(terceiro_numero)).replace('.', ','))
+
 
 def calcula_peso_ideal(sexo, altura):
     peso_ideal = 0
@@ -80,6 +86,7 @@ def ex_8():
     else:
         return print('Seu peso idela é {:.2f}kg'.format(calculo_peso_ideal).replace('.', ','))
 
+
 def calcula_peso_excedente_dia(peso):
     peso_maximo = 50
     peso_excedente_dia = float(peso) - peso_maximo
@@ -108,6 +115,31 @@ def ex_9(peso_dia):
     return print('Você excedeu {0:.2f}kg e deverá pagar uma multa de R${1:.2f}.'.format(peso_excedente, multa_dia).replace('.', ','))
 
 
+def ex_10():
+    #10.	Faça um Programa que pergunte quanto você ganha por hora e 
+    # o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário 
+    # no referido mês, sabendo-se que são descontados 11% para o Imposto de Renda, 
+    # 8% para o INSS e 5% para o sindicato, faça um programa que nos dê:
+    # a.	salário bruto.
+    # b.	quanto pagou ao INSS.
+    # c.	quanto pagou ao sindicato.
+    # d.	o salário líquido.
+    # Calcule os descontos e o salário líquido, conforme a tabela abaixo:
+    # + Salário Bruto : R$
+    # - IR (11%) : R$
+    # - INSS (8%) : R$
+    # - Sindicato ( 5%) : R$
+    # = Salário Liquido : R$
+    # Obs.: Salário Bruto - Descontos = Salário Líquido.
+
+    ganho_hora = float(input('Informe quanto você ganha por hora: '))
+    qtd_horas = int(input('Informe a quantidade de horas trabalhadas no mês: '))
+    
+    salario_bruto = calcula_salario(qtd_horas, ganho_hora)
+
+    return print('Seu salário bruto é R${:.2f}'.format(salario_bruto).replace('.', ','))
+
+
 def run():
     ex_1(input('Digite o tamanho desejado em centímetros: '))
     ex_2(input('Digite o raio do círculo que deseja calcular sua área: '))
@@ -116,5 +148,7 @@ def run():
     ex_6(input('Digite o grau Farenheit que deseja converter: '))
     ex_7()
     ex_8()
+    ex_9(input('Digite o peso (em kg) dos peixes adquiridos hoje: '))
 
-ex_9(input('Digite o peso (em kg) dos peixes adquiridos hoje: '))
+
+ex_10()
