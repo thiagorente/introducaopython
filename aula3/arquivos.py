c671d48c2a9042c19979b64c1b652fb372d6c281
Thiago Silva Rente <thiagorente@MacBook-Air-de-Thiago.local>
2019-11-20 17:12:30 -0300

    enunciados

--- aula3/arquivos.py
+++ aula3/arquivos.py
@@ -1,4 +1,7 @@
 def ex_1():
+    #Faça um programa que leia um arquivo texto contendo uma lista de endereços IP e gere um outro arquivo, 
+    # contendo um relatório dos endereços IP válidos e inválidos.
+
     lista_ips = open('aula3/resources/lista_ip.txt', 'r')
     resultado = open('aula3/output/resultado.txt', 'w')
 
@@ -48,6 +51,17 @@ def calcula_percentual(valor_inferior, valor_total):
     return ((100 * valor_inferior) / valor_total)
 
 def ex_2():
+    # A ACME Inc., uma empresa de 500 funcionários, está tendo problemas de espaço em disco no seu servidor de arquivos. 
+    # Para tentar resolver este problema, o Administrador de Rede precisa saber qual o espaço ocupado pelos usuários, 
+    # e identificar os usuários com maior espaço ocupado. Através de um programa, baixado da Internet, 
+    # ele conseguiu gerar o seguinte arquivo, chamado "usuarios.txt
+    # Neste arquivo, o nome do usuário possui 15 caracteres. A partir deste arquivo, 
+    # você deve criar um programa que gere um relatório, chamado "relatório.txt", no seguinte formato:
+    # O arquivo de entrada deve ser lido uma única vez, e os dados armazenados em memória, caso sejam necessários, 
+    # de forma a agilizar a execução do programa. A conversão do espaço ocupado em disco, de bytes para megabytes 
+    # deverá ser feita através de uma função separada, que será chamada pelo programa principal. 
+    # O cálculo do percentual de uso também deverá ser feito através de uma função, que será chamada pelo programa principal.
+    
     usuarios = open('aula3/resources/usuarios.txt', 'r')
     relatorio = open('aula3/output/relatorio.txt', 'w')
 
